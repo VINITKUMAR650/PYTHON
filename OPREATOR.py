35d30eaf3e5a9279b131f50bad60e4bf25f085ea
VINITKUMAR650 <114825681+VINITKUMAR650@users.noreply.github.com>
2022-10-15 13:39:13 +0530

    Add files via upload

---
+++ OPREATOR.py
@@ -0,0 +1,89 @@
+# python operators #
+
+# arithemetic operators
+
+# print(10-20)  # substraction
+# print(10+30)  # addition
+# print(10*3)  # multiply
+# print(20/10)  # dividsion
+# print(20 % 100)  # modules gives the reminder
+# print(15//2)  # flour Division
+# print(15//4)
+
+#x = 2
+#y = 3
+
+# print(2**3)  # exponential
+
+
+# ASSIGNMENT OPERATOR
+# a = 20  # =
+# a += 5  # +=  It means same as a=a+5
+# a -= 5  # -=  It means same as a=a-5
+# a *= 5  # *=  It means same as a=a*5
+# a /= 5  # /=  It means same as a=a/5
+# a %= 5  # %=  It means same as a=a%5
+# a //= 5  #  It means same as a=a//5
+# a **= 5  #  **= It means samse as a=a**5
+
+# print(a)
+
+
+# COMPARISON OPERATOR (True , False)
+
+# print(3 == 4)  # equal to
+
+# print(3 != 4)  # not equal to
+
+# print(3 < 4) # less than
+
+# print(3 > 4) # greater than
+
+# print(3 <= 4) # less than equal to
+
+# print(3 >= 4) # greater than equal to
+
+# print(4 <= 4) # less than equal to
+
+# print(4 < 4) # less than
+
+
+# LOGICAL OPERATORS (OR, AND,NOT)
+
+#x = 2
+
+# (AND) OPERATOR
+#print(x < 5 and x < 8)
+#print(x > 5 and x > 8)
+
+# (OR) OPERATOR
+
+#x = 2
+
+#print(x < 5 or x < 8)
+
+#print(x > 5 or x > 10)
+
+
+# (NOT) OPERATOR
+
+#x = 2
+
+#print(not(x < 4, x > 8))
+
+# print(not(x=5, x=10))
+
+# IDENTITY OPERATOR
+#a = [10, 20]
+#b = [10, 20]
+#c = a
+
+# a.remove(20)
+
+# print(a)
+# print(b)
+# print(c)
+
+#print(a is c)
+
+#print(b is c)
